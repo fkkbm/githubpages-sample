62b539072fd8859e931c99398affc18d1427225a
kubo20002001 <kubo20002001@gmail.com>
2021-11-06 00:17:11 +0900

    vue-routerつかってみた

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,39 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <v-btn class="ma-2">
+        <router-link to="/">Home</router-link><v-icon small>mdi-home</v-icon>
+      </v-btn>
+      <v-btn class="ma-2">
+        <router-link to="/about">About</router-link><v-icon small>mdi-pencil</v-icon>
+      </v-btn>
+      <v-btn class="ma-2">
+        <router-link to="/history">History</router-link><v-icon small>mdi-book</v-icon>
+      </v-btn>
+    </div>
+    <router-view />
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
