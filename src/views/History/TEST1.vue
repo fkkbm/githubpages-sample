e08b669e26efecfa12420e491e04386232a3a6a0
kubo20002001 <kubo20002001@gmail.com>
2021-11-07 23:20:49 +0900

    サイズ調整

--- src/views/History/TEST1.vue
+++ src/views/History/TEST1.vue
@@ -2,49 +2,43 @@
   <div class="test1">
     <v-container>
       <v-row class="grey lighten-3" style="height: 100px">
-        <v-col cols="12" sm="6" md="4" lg="3" xl="3" style="background-color: #ffcdd2">
+        <v-col cols="12" sm="6" md="4" lg="3" xl="2" style="background-color: #ffcdd2">
           row1 column 1
         </v-col>
-        <v-col cols="12" sm="6" md="4" lg="3" xl="3" style="background-color: #f8bbd0">
+        <v-col cols="12" sm="6" md="4" lg="3" xl="2" style="background-color: #f8bbd0">
           row1 column 2
         </v-col>
-        <v-col cols="12" sm="6" md="4" lg="3" xl="3" style="background-color: #e1bee7">
+        <v-col cols="12" sm="6" md="4" lg="3" xl="2" style="background-color: #e1bee7">
           row1 column 3
         </v-col>
-        <v-col cols="12" sm="6" md="4" lg="3" xl="3" style="background-color: #d1c4e9">
+        <v-col cols="12" sm="6" md="4" lg="3" xl="2" style="background-color: #d1c4e9">
           row1 column 4
         </v-col>
-        <v-col cols="12" sm="6" md="4" lg="3" xl="3" style="background-color: #c5cae9">
+        <v-col cols="12" sm="6" md="4" lg="3" xl="2" style="background-color: #c5cae9">
           row1 column 5
         </v-col>
-        <v-col cols="12" sm="6" md="4" lg="3" xl="3" style="background-color: #bbdefb">
+        <v-col cols="12" sm="6" md="4" lg="3" xl="2" style="background-color: #bbdefb">
           row1 column 6
         </v-col>
-        <v-col cols="12" sm="6" md="4" lg="3" xl="3" style="background-color: #b3e5fc">
-          row1 column 7
-        </v-col>
       </v-row>
-      <v-row class="grey lighten-3" style="height: 200px">
-        <v-col cols="6" sm="6" md="4" lg="3" xl="3" style="background-color: #ffcdd2">
+      <v-row class="grey lighten-3" style="height: 100px">
+        <v-col cols="6" sm="4" md="3" lg="2" xl="1" style="background-color: #ffcdd2">
           row2 column 1
         </v-col>
-        <v-col cols="6" sm="6" md="4" lg="3" xl="3" style="background-color: #f8bbd0">
+        <v-col cols="6" sm="4" md="3" lg="2" xl="1" style="background-color: #f8bbd0">
           row2 column 2
         </v-col>
-        <v-col cols="6" sm="6" md="4" lg="3" xl="3" style="background-color: #e1bee7">
+        <v-col cols="6" sm="4" md="3" lg="2" xl="1" style="background-color: #e1bee7">
           row2 column 3
         </v-col>
-        <v-col cols="6" sm="6" md="4" lg="3" xl="3" style="background-color: #d1c4e9">
+        <v-col cols="6" sm="4" md="3" lg="2" xl="1" style="background-color: #d1c4e9">
           row2 column 4
         </v-col>
-        <v-col cols="6" sm="6" md="4" lg="3" xl="3" style="background-color: #c5cae9">
+        <v-col cols="6" sm="4" md="3" lg="2" xl="1" style="background-color: #c5cae9">
           row2 column 5
         </v-col>
-        <v-col cols="6" sm="6" md="4" lg="3" xl="3" style="background-color: #bbdefb">
+        <v-col cols="6" sm="4" md="3" lg="2" xl="1" style="background-color: #bbdefb">
           row2 column 6
-        </v-col>
-        <v-col cols="6" sm="6" md="4" lg="3" xl="3" style="background-color: #b3e5fc">
-          row2 column 7
         </v-col>
       </v-row>
     </v-container>
