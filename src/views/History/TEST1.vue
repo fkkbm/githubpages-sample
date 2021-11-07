e65c00d81df9ca834bf1eec5308525f00d5a84e7
kubo20002001 <kubo20002001@gmail.com>
2021-11-07 23:11:22 +0900

    grid-sysytemお試し

--- src/views/History/TEST1.vue
+++ src/views/History/TEST1.vue
@@ -1,5 +1,52 @@
 <template>
   <div class="test1">
-    test1
+    <v-container>
+      <v-row class="grey lighten-3" style="height: 300px" stype="background-color: 'grey'">
+        <v-col cols="12" sm="6" md="4" lg="3" xl="3" style="background-color: #ffcdd2">
+          row1 column 1
+        </v-col>
+        <v-col cols="12" sm="6" md="4" lg="3" xl="3" style="background-color: #f8bbd0">
+          row1 column 2
+        </v-col>
+        <v-col cols="12" sm="6" md="4" lg="3" xl="3" style="background-color: #e1bee7">
+          row1 column 3
+        </v-col>
+        <v-col cols="12" sm="6" md="4" lg="3" xl="3" style="background-color: #d1c4e9">
+          row1 column 4
+        </v-col>
+        <v-col cols="12" sm="6" md="4" lg="3" xl="3" style="background-color: #c5cae9">
+          row1 column 5
+        </v-col>
+        <v-col cols="12" sm="6" md="4" lg="3" xl="3" style="background-color: #bbdefb">
+          row1 column 6
+        </v-col>
+        <v-col cols="12" sm="6" md="4" lg="3" xl="3" style="background-color: #b3e5fc">
+          row1 column 7
+        </v-col>
+      </v-row>
+      <v-row class="grey lighten-3" style="height: 200">
+        <v-col cols="6" sm="6" md="4" lg="3" xl="3" style="background-color: #ffcdd2">
+          row2 column 1
+        </v-col>
+        <v-col cols="6" sm="6" md="4" lg="3" xl="3" style="background-color: #f8bbd0">
+          row2 column 2
+        </v-col>
+        <v-col cols="6" sm="6" md="4" lg="3" xl="3" style="background-color: #e1bee7">
+          row2 column 3
+        </v-col>
+        <v-col cols="6" sm="6" md="4" lg="3" xl="3" style="background-color: #d1c4e9">
+          row2 column 4
+        </v-col>
+        <v-col cols="6" sm="6" md="4" lg="3" xl="3" style="background-color: #c5cae9">
+          row2 column 5
+        </v-col>
+        <v-col cols="6" sm="6" md="4" lg="3" xl="3" style="background-color: #bbdefb">
+          row2 column 6
+        </v-col>
+        <v-col cols="6" sm="6" md="4" lg="3" xl="3" style="background-color: #b3e5fc">
+          row2 column 7
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
